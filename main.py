2cc7de4f5c9d91ab0fa772fa00758fef7f96419d
kento13410 <kento13410@gmail.com>
2023-03-05 15:44:15 +0900

    ver1

---
+++ main.py
@@ -0,0 +1,42 @@
+import openai
+
+
+def getOpenAIResponse(prompt):
+    openai.api_key = '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'  # 発行したAPIキーを入力
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,  # 入力する文章
+        max_tokens=1000,  # 文章の最大単語数
+        echo=False  # promptの文章を出力
+    )
+
+    return response['choices'][0]['text'].replace('¥n', '')
+
+
+class WordPressFormat:
+    def __init__(self, item, request, response, htmlformat):
+        self.item = item
+        self.request = request
+        self.response = response
+        self.htmlformat = htmlformat
+
+    def getFormatContent(self):
+        if self.item != 'タイトル':
+            return '<' + self.htmlformat + '>' + self.response + '</' + self.htmlformat + '>'
+        else:
+            return self.response
+
+
+title = ''
+blog_element_list = [
+    WordPressFormat('タイトル', 'ChatGPTとプログラミングを用いた副業のタイトルを考えてください', '', ''),
+    WordPressFormat('リード文', 'のリード文を考えてください', '', 'p'),
+    WordPressFormat('本文', 'の本文をこれまでの流れを踏まえ、考えてください', '', 'p'),
+    WordPressFormat('まとめ', 'のまとめをこれまでの流れを踏まえ、考えてください', '', 'p'),
+]
+for blog_element in blog_element_list:
+    blog_element.response = getOpenAIResponse(title + blog_element.request)
+    if title == '':
+        title = blog_element.response.replace('/n', '')
+    print(blog_element.getFormatContent())
